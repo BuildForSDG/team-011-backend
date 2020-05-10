eefa8d41c615e2713de3e8f2366431a8a6094e46
Fortune <fortuneochi@gmail.com>
2020-05-10 03:26:17 +0100

    fix: extend jest default timeout to allow setup time

--- __tests__/jest.setup.js
+++ __tests__/jest.setup.js
@@ -1,16 +1,1 @@
-// const dbHandler = require('./db-handler');
-
-// /**
-//  * Connect to a new in-memory database before running any tests.
-//  */
-// beforeAll(async () => dbHandler.connect());
-
-// /**
-//  * Clear all test data after every test.
-//  */
-// afterEach(async () => dbHandler.clearDatabase());
-
-// /**
-//  * Remove and close the db and server.
-//  */
-// afterAll(async () => dbHandler.closeDatabase());
+jest.setTimeout(10000); // in milliseconds
