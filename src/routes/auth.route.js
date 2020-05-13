96e0e7c1e1de7a5336def7b5073b235ea819a15f
Fortune <fortuneochi@gmail.com>
2020-05-13 04:09:28 +0100

    fix: lint issue

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -15,6 +15,7 @@ router.post(
   '/register',
   [
     check('email').isEmail().withMessage('Enter a valid email address'),
+    // eslint-disable-next-line newline-per-chained-call
     check('password').not().isEmpty().isLength({ min: 6 }).withMessage('Must be at least 6 chars long'),
     check('firstName').not().isEmpty().withMessage('You first name is required'),
     check('lastName').not().isEmpty().withMessage('You last name is required')
@@ -47,6 +48,7 @@ router.get('/reset/:token', Password.reset);
 router.post(
   '/reset/:token',
   [
+    // eslint-disable-next-line newline-per-chained-call
     check('password').not().isEmpty().isLength({ min: 6 }).withMessage('Must be at least 6 chars long'),
     check('confirmPassword', 'Passwords do not match').custom((value, { req }) => value === req.body.password)
   ],
