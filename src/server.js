e9b9a46d24c966d324d385d1ea7c18f2ae3a91d0
Fortune <fortuneochi@gmail.com>
2020-05-14 23:51:23 +0100

    feat: creation of farmland with farm image. Closes #6

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app.js');
 
-const port = process.env.NODE_ENV || 3000;
+const port = process.env.NODE_ENV || 1234;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
